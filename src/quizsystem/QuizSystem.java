7003ab3be9eb9e6b03e812d63295560afa2c5f0d
zartabkhalil <zartabkhalil11@gmail.com>
2020-12-20 14:31:01 +0500

    i have completed Design till students section

---
+++ src/quizsystem/QuizSystem.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quizsystem;
+
+/**
+ *
+ * @author zakisardar
+ */
+public class QuizSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      SplashScreen first_layout = new SplashScreen();
+      first_layout.setVisible(true);
+     
+      first_layout.iterate();
+      
+      
+       
+    }
+    
+}
